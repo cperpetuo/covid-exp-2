94db604a4dc49cad022ace613e6e2f23a1f14a9f
cperpetuo <71944260+cperpetuo@users.noreply.github.com>
2020-09-28 16:49:40 -0300

    Update questions.js

--- questions.js
+++ questions.js
@@ -8,175 +8,150 @@ var questions = [
     },
     {
         id: 2,
-        title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sit amet leo maximus, sagittis justo a, vulputate tellus. Aliquam fringilla.',
         description: 'Você só pode espalhar o coronavírus se estiver doente.',
         options: '1-7',
         type: 'otimista'
     },
     {
         id: 3,
-        title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sit amet leo maximus, sagittis justo a, vulputate tellus. Aliquam fringilla.',
         description: 'O coronavírus não sobrevive em plástico ou aço por mais de alguns minutos.',
         options: '1-7',
         type: 'otimista'
     },
     {
         id: 4,
-        title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sit amet leo maximus, sagittis justo a, vulputate tellus. Aliquam fringilla.',
         description: 'Os sintomas do coronavírus têm vida curta.',
         options: '1-7',
        type: 'otimista'
     },
     {
         id: 5,
-        title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sit amet leo maximus, sagittis justo a, vulputate tellus. Aliquam fringilla.',
         description: 'O clima quente impede efetivamente a propagação do coronavírus.',
         options: '1-7',
       type: 'otimista'
     },
     {
         id: 6,
-        title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sit amet leo maximus, sagittis justo a, vulputate tellus. Aliquam fringilla.',
         description: 'Você pode dizer quase imediatamente (dentro de um dia) se você contraiu o coronavírus.',
         options: '1-7',
         type: 'otimista'
     },
     {
         id: 7,
-        title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sit amet leo maximus, sagittis justo a, vulputate tellus. Aliquam fringilla.',
         description: 'O vírus é relativamente grande, então qualquer tipo de máscara pode filtrá-lo.',
         options: '1-7',
         type: 'otimista'
     },
     {
         id: 8,
-        title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sit amet leo maximus, sagittis justo a, vulputate tellus. Aliquam fringilla.',
         description: 'O coronavírus permanece vivo na sua mão por cerca de 5 a 10 minutos.',
         options: '1-7',
         type: 'otimista'
     },
     {
         id: 9,
-        title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sit amet leo maximus, sagittis justo a, vulputate tellus. Aliquam fringilla.',
         description: 'O coronavírus não é transportado pelo ar.',
         options: '1-7',
         type: 'otimista'
     },
     {
         id: 10,
-        title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sit amet leo maximus, sagittis justo a, vulputate tellus. Aliquam fringilla.',
         description: 'Cães e gatos podem contrair e espalhar o coronavírus.',
         options: '1-7',
         type: 'pessimista'
     },
     {
         id: 11,
-        title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sit amet leo maximus, sagittis justo a, vulputate tellus. Aliquam fringilla.',
         description: 'A grande maioria das pessoas que contraem o coronavírus precisará ser hospitalizada.',
         options: '1-7',
         type: 'pessimista'
     },
     {
         id: 12,
-        title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sit amet leo maximus, sagittis justo a, vulputate tellus. Aliquam fringilla.',
         description: 'O coronavírus mata a maioria das pessoas que o contraem.',
         options: '1-7',
         type: 'pessimista'
     },
     {
         id: 13,
-        title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sit amet leo maximus, sagittis justo a, vulputate tellus. Aliquam fringilla.',
         description: 'A maioria das pessoas provavelmente entrará em contato com o coronavírus simplesmente saindo de casa e dando uma volta.',
         options: '1-7',
         type: 'pessimista'
     },
     {
         id: 14,
-        title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sit amet leo maximus, sagittis justo a, vulputate tellus. Aliquam fringilla.',
         description: 'A vitamina C pode curar o coronavírus.',
         options: '1-7',
         type: 'mistico'
     },
     {
         id: 15,
-        title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sit amet leo maximus, sagittis justo a, vulputate tellus. Aliquam fringilla.',
         description: 'Prender a respiração por 10 segundos sem desconforto é uma maneira eficaz de testar se você tem coronavírus.',
         options: '1-7',
         type: 'mistico'
     },
     {
         id: 16,
-        title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sit amet leo maximus, sagittis justo a, vulputate tellus. Aliquam fringilla.',
         description: 'A hidroxicloroquina mostrou-se um medicamento eficaz no tratamento do coronavírus.',
         options: '1-7',
         type: 'mistico'
     },
     {
         id: 17,
-        title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sit amet leo maximus, sagittis justo a, vulputate tellus. Aliquam fringilla.',
         description: 'Ivermectina ajuda na prevenção ou cura do coronavírus.',
         options: '1-7',
         type: 'mistico'
     },
     {
         id: 18,
-        title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sit amet leo maximus, sagittis justo a, vulputate tellus. Aliquam fringilla.',
         description: 'O gargarejo com água morna e sal ou vinagre, elimina o coronavírus.',
         options: '1-7',
         type: 'mistico'
     },
     {
         id: 19,
-        title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sit amet leo maximus, sagittis justo a, vulputate tellus. Aliquam fringilla.',
         description: 'O coronavírus foi criado em um laboratório.',
         options: '1-7',
         type: 'conspiratoria'
     },
     {
         id: 20,
-        title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sit amet leo maximus, sagittis justo a, vulputate tellus. Aliquam fringilla.',
         description: 'O coronavírus foi criado para ser uma arma biológica.',
         options: '1-7',
         type: 'conspiratoria'
     },
     {
         id: 21,
-        title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sit amet leo maximus, sagittis justo a, vulputate tellus. Aliquam fringilla.',
         description: 'Uma cura para o coronavírus já foi descoberta, mas está sendo suprimida por pessoas que desejam que a pandemia continue.',
         options: '1-7',
         type: 'conspiratoria'
     },
     {
         id: 22,
-        title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sit amet leo maximus, sagittis justo a, vulputate tellus. Aliquam fringilla.',
         description: 'O coronavírus é provavelmente uma farsa.',
         options: '1-7',
         type: 'conspiratoria'
     },
     {
         id: 23,
-        title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sit amet leo maximus, sagittis justo a, vulputate tellus. Aliquam fringilla.',
         description: 'As ondas eletromagnéticas da tecnologia do 5G enfraquecem a imunidade e aumentam o risco de contrair o coronavírus.',
         options: '1-7',
         type: 'conspiratoria'
     }, 
     {
         id: 24,
-        title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sit amet leo maximus, sagittis justo a, vulputate tellus. Aliquam fringilla.',
         description: 'O coronavírus foi inventado para esconder os perigos do 5G.',
         options: '1-7',
         type: 'conspiratoria'
     },
     {
         id: 25,
-        title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sit amet leo maximus, sagittis justo a, vulputate tellus. Aliquam fringilla.',
         description: 'A vacina está sendo desenvolvida para o COVID-19 com o objetivo de injetar microchips nas pessoas.',
         options: '1-7',
         type: 'conspiratoria'
     },
     {
         id: 3,
-        title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sit amet leo maximus, sagittis justo a, vulputate tellus. Aliquam fringilla.',
         description: 'O coronavírus não sobrevive em plástico ou aço por mais de alguns minutos.',
         options: 'yes_no',
         type: 'ciencia',
