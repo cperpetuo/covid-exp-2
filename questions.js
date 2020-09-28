732fee1c729677c777b30dd791d3ac0755e6ce96
cperpetuo <71944260+cperpetuo@users.noreply.github.com>
2020-09-28 16:14:49 -0300

    Update questions.js

--- questions.js
+++ questions.js
@@ -63,6 +63,122 @@ var questions = [
         type: 'otimista'
     },
     {
+        id: 10,
+        title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sit amet leo maximus, sagittis justo a, vulputate tellus. Aliquam fringilla.',
+        description: 'Cães e gatos podem contrair e espalhar o coronavírus.',
+        options: '1-7',
+        type: 'pessimista'
+    },
+    {
+        id: 11,
+        title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sit amet leo maximus, sagittis justo a, vulputate tellus. Aliquam fringilla.',
+        description: 'A grande maioria das pessoas que contraem o coronavírus precisará ser hospitalizada.',
+        options: '1-7',
+        type: 'pessimista'
+    },
+    {
+        id: 12,
+        title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sit amet leo maximus, sagittis justo a, vulputate tellus. Aliquam fringilla.',
+        description: 'O coronavírus mata a maioria das pessoas que o contraem.',
+        options: '1-7',
+        type: 'pessimista'
+    },
+    {
+        id: 13,
+        title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sit amet leo maximus, sagittis justo a, vulputate tellus. Aliquam fringilla.',
+        description: 'A maioria das pessoas provavelmente entrará em contato com o coronavírus simplesmente saindo de casa e dando uma volta.',
+        options: '1-7',
+        type: 'pessimista'
+    },
+    {
+        id: 14,
+        title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sit amet leo maximus, sagittis justo a, vulputate tellus. Aliquam fringilla.',
+        description: 'A vitamina C pode curar o coronavírus.',
+        options: '1-7',
+        type: 'mistico'
+    },
+    {
+        id: 15,
+        title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sit amet leo maximus, sagittis justo a, vulputate tellus. Aliquam fringilla.',
+        description: 'Prender a respiração por 10 segundos sem desconforto é uma maneira eficaz de testar se você tem coronavírus.',
+        options: '1-7',
+        type: 'mistico'
+    },
+    {
+        id: 16,
+        title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sit amet leo maximus, sagittis justo a, vulputate tellus. Aliquam fringilla.',
+        description: 'A hidroxicloroquina mostrou-se um medicamento eficaz no tratamento do coronavírus.',
+        options: '1-7',
+        type: 'mistico'
+    },
+    {
+        id: 17,
+        title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sit amet leo maximus, sagittis justo a, vulputate tellus. Aliquam fringilla.',
+        description: 'Ivermectina ajuda na prevenção ou cura do coronavírus.',
+        options: '1-7',
+        type: 'mistico'
+    },
+    {
+        id: 18,
+        title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sit amet leo maximus, sagittis justo a, vulputate tellus. Aliquam fringilla.',
+        description: 'O gargarejo com água morna e sal ou vinagre, elimina o coronavírus.',
+        options: '1-7',
+        type: 'mistico'
+    },
+    {
+        id: 19,
+        title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sit amet leo maximus, sagittis justo a, vulputate tellus. Aliquam fringilla.',
+        description: 'O coronavírus foi criado em um laboratório.',
+        options: '1-7',
+        type: 'conspiratoria'
+    },
+    {
+        id: 20,
+        title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sit amet leo maximus, sagittis justo a, vulputate tellus. Aliquam fringilla.',
+        description: 'O coronavírus foi criado para ser uma arma biológica.',
+        options: '1-7',
+        type: 'conspiratoria'
+    },
+    {
+        id: 21,
+        title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sit amet leo maximus, sagittis justo a, vulputate tellus. Aliquam fringilla.',
+        description: 'Uma cura para o coronavírus já foi descoberta, mas está sendo suprimida por pessoas que desejam que a pandemia continue.',
+        options: '1-7',
+        type: 'conspiratoria'
+    },
+    {
+        id: 22,
+        title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sit amet leo maximus, sagittis justo a, vulputate tellus. Aliquam fringilla.',
+        description: 'O coronavírus é provavelmente uma farsa.',
+        options: '1-7',
+        type: 'conspiratoria'
+    },
+    {
+        id: 23,
+        title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sit amet leo maximus, sagittis justo a, vulputate tellus. Aliquam fringilla.',
+        description: 'As ondas eletromagnéticas da tecnologia do 5G enfraquecem a imunidade e aumentam o risco de contrair o coronavírus.',
+        options: '1-7',
+        type: 'conspiratoria'
+    }, 
+    {
+        id: 24,
+        title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sit amet leo maximus, sagittis justo a, vulputate tellus. Aliquam fringilla.',
+        description: 'O coronavírus foi inventado para esconder os perigos do 5G.',
+        options: '1-7',
+        type: 'conspiratoria'
+    },
+    {
+        id: 25,
+        title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sit amet leo maximus, sagittis justo a, vulputate tellus. Aliquam fringilla.',
+        description: 'A vacina está sendo desenvolvida para o COVID-19 com o objetivo de injetar microchips nas pessoas.',
+        options: '1-7',
+        type: 'conspiratoria'
+    },
+    
+     
+    
+       
+    {
         id: 3,
         title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sit amet leo maximus, sagittis justo a, vulputate tellus. Aliquam fringilla.',
         description: 'O coronavírus não sobrevive em plástico ou aço por mais de alguns minutos.',
