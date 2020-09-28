97e881910eec204ff4375927496fabf882cc4d5d
cperpetuo <71944260+cperpetuo@users.noreply.github.com>
2020-09-28 15:40:37 -0300

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,22 +1,63 @@
 var questions = [
     {
         id: 1,
-        title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sit amet leo maximus, sagittis justo a, vulputate tellus. Aliquam fringilla.',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut volutpat laoreet ipsum, eget dignissim sapien ultrices quis. Pellentesque eu viverra eros, a congue diam. Cras et iaculis arcu, non commodo ante. Curabitur id varius eros. Curabitur consequat consequat ipsum vel accumsan. Fusce sollicitudin neque at nisl interdum, id malesuada neque suscipit. Mauris vitae ultrices felis.',
+        title: 'Para responder as questões a seguir, use a escala fornecida abaixo, pensando na sua percepção em relação ao COVID-19.',
+        description: 'A gripe sazonal é tão perigosa quanto o coronavírus.',
         options: '1-7',
-        type: 'mistico'
+        type: 'otimista'
     },
     {
         id: 2,
         title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sit amet leo maximus, sagittis justo a, vulputate tellus. Aliquam fringilla.',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut volutpat laoreet ipsum, eget dignissim sapien ultrices quis. Pellentesque eu viverra eros, a congue diam. Cras et iaculis arcu, non commodo ante. Curabitur id varius eros. Curabitur consequat consequat ipsum vel accumsan. Fusce sollicitudin neque at nisl interdum, id malesuada neque suscipit. Mauris vitae ultrices felis.',
-        options: '1-7',
-        type: 'ciencia'
+        description: 'Você só pode espalhar o coronavírus se estiver doente.',
+        type: 'otimista'
     },
     {
         id: 3,
         title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sit amet leo maximus, sagittis justo a, vulputate tellus. Aliquam fringilla.',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut volutpat laoreet ipsum, eget dignissim sapien ultrices quis. Pellentesque eu viverra eros, a congue diam. Cras et iaculis arcu, non commodo ante. Curabitur id varius eros. Curabitur consequat consequat ipsum vel accumsan. Fusce sollicitudin neque at nisl interdum, id malesuada neque suscipit. Mauris vitae ultrices felis.',
+        description: 'O coronavírus não sobrevive em plástico ou aço por mais de alguns minutos.',
+        type: 'otimista'
+    },
+    {
+        id: 4,
+        title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sit amet leo maximus, sagittis justo a, vulputate tellus. Aliquam fringilla.',
+        description: 'Os sintomas do coronavírus têm vida curta.',
+        type: 'otimista'
+    },
+    {
+        id: 5,
+        title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sit amet leo maximus, sagittis justo a, vulputate tellus. Aliquam fringilla.',
+        description: 'O clima quente impede efetivamente a propagação do coronavírus.',
+        type: 'otimista'
+    },
+    {
+        id: 6,
+        title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sit amet leo maximus, sagittis justo a, vulputate tellus. Aliquam fringilla.',
+        description: 'Você pode dizer quase imediatamente (dentro de um dia) se você contraiu o coronavírus.',
+        type: 'otimista'
+    },
+    {
+        id: 7,
+        title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sit amet leo maximus, sagittis justo a, vulputate tellus. Aliquam fringilla.',
+        description: 'O vírus é relativamente grande, então qualquer tipo de máscara pode filtrá-lo.',
+        type: 'otimista'
+    },
+    {
+        id: 8,
+        title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sit amet leo maximus, sagittis justo a, vulputate tellus. Aliquam fringilla.',
+        description: 'O coronavírus permanece vivo na sua mão por cerca de 5 a 10 minutos.',
+        type: 'otimista'
+    },
+    {
+        id: 9,
+        title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sit amet leo maximus, sagittis justo a, vulputate tellus. Aliquam fringilla.',
+        description: 'O coronavírus não é transportado pelo ar.',
+        type: 'otimista'
+    },
+    {
+        id: 3,
+        title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sit amet leo maximus, sagittis justo a, vulputate tellus. Aliquam fringilla.',
+        description: 'O coronavírus não sobrevive em plástico ou aço por mais de alguns minutos.',
         options: 'yes_no',
         type: 'ciencia',
         image: 'images/fakenews.jpg'
