f179c7c67381357b8982057402f164e94814b201
cperpetuo <71944260+cperpetuo@users.noreply.github.com>
2021-03-06 15:34:16 -0300

    Add files via upload

--- questions.js
+++ questions.js
@@ -85,72 +85,81 @@ var questions = [
         category: ''
     }];
     
-var questions_intencao_vacina= [    
+var questions_intencao_vacina_antes= [    
     {
-        id: 201,
+        id: 20,
         title: 'Abaixo estão algumas frases que você deve avaliar de acordo com sua opinião. Use a escala de avaliação fornecida abaixo, na qual o ponto 1 significa que você discorda totalmente da frase, e o ponto 7 indica que você concorda totalmente com ela',
         description: 'Qual a sua intenção de  tomar a vacina e vacinar sua família contra a covid-19 quando estiver disponível?',
         options: '1-7',
-        category: ''
+        category: 'intencao_antes'
     },
+	{
+		id: 21,
+        description: 'Quando você pretende se vacinar contra a covid-19?',
+        list:['o quanto antes','em até seis meses', 'após seis meses', 'nunca'],
+        category: 'intencao_tempo'
+	}
     {
-        id: 202,
+        id: 22,
         description: 'Baseado em seus conhecimentos, quão provável que ao se vacinar contra a covid-19 você venha a sofrer complicações de saúde?',
         options: '1-7',
-        category: ''
+        category: 'complicacoes_antes'
     }];
    
-var questios_ciencia= [
+var questions_ciencia= [
     {
-        id: 207,
+        id: 30,
         description: 'A ciência nos fornece uma melhor compreensão do universo do que a religião. ',
         options: '1-7',
         category: 'ciencia'
     },
     {
-        id: 208,
+        id: 31,
         description: 'O método científico é o melhor caminho confiável para o conhecimento.',
         options: '1-7',
         category: 'ciencia'
     },
     {
-        id: 209,
+        id: 32,
         description: 'O real conhecimento que podemos ter é o conhecimento científico. ',
         options: '1-7',
         category: 'ciencia'
     },
     {
-        id: 210,
+        id: 33,
         description: 'A ciência é a parte mais valiosa da cultura humana.',
         options: '1-7',
         category: 'ciencia'
     },
     {
-        id: 211,
+        id: 34,
         description: 'A ciência é o meio mais eficiente de alcançar a cura para o COVID-19.',
         options: '1-7',
         category: 'ciencia'
-    },
+    }
+]; 
+
+var questions_distanciamento= [
     {
-        id: 212,
+        id: 40,
         description: 'Evitei intencionalmente qualquer forma de aglomeração social devido ao COVID-19. ',
         options: '1-7',
         category: 'distanciamento'
     },   
     {
-        id: 213,
+        id: 41,
         description: 'Em geral, segui as diretrizes do governo relacionadas ao distanciamento social. ',
         options: '1-7',
         category: 'distanciamento'
     },   
     {
-        id: 214,
+        id: 42,
         description: 'Eu mantenho uma distância de 1,5m de outras pessoas fora da minha família imediata por causa do COVID-19.',
         options: '1-7',
         category: 'distanciamento'
     },   
     {
-        id: 215,
+        id: 43,
         description: 'O distanciamento social é uma medida eficiente para o controle do COVID-19. ',
         options: '1-7',
         category: 'distanciamento'
@@ -160,7 +169,7 @@ var questios_ciencia= [
     
 var demograficas = [    
     {
-        id: 301,
+        id: 51,
         title: 'As questões a seguir tratam do seu perfil pessoal. Sinta-se à vontade para responder, lembrando que seus dados serão tratados de forma confidencial de forma a preservar sua privacidade e o sigilo das informações disponibilizadas.',
         description: 'Sexo:',
         options: 'list',
@@ -168,63 +177,63 @@ var demograficas = [
         category: 'perfil_respondente'
     },
     {
-        id: 302,
+        id: 52,
         description: 'Idade:',
         options: 'list',
         list: ['Entre 20 e 29 anos','Entre 30 e 39 anos','Entre 40 e 49 anos','Entre 50 e 59 anos','Acima de 60 anos'],
         category: 'perfil_respondente'
     },
     {
-        id: 303,
+        id: 53,
         description: 'Renda Familiar Bruta Mensal (incluindo todas as fontes de renda, tais como: salários, comissões, aluguéis, outros)',
         options: 'list',
         list: ['até R$ 1.874,00','R$ 1.874,01 a R$ 3.748,00','R$ 3.748,01 a R$ 9.370,00','R$ 9.370,01 a R$ 18.740,00','Acima de R$ 18.740,00'],
         category: 'perfil_respondente'
     },  
     {
-        id: 304,
+        id: 54,
         description: 'Qual o tamanho da população onde você mora?',
         options: 'list',
         list: ['mais de 10 M habitantes','5.1 M – 10 M habitantes','1.1 M  - 5 M habitantes','menos de 1 M habitantes'],
         category: 'perfil_respondente'
     },  
     {
-        id: 305,
+        id: 55,
         description: 'Qual é o seu nível de escolaridade?',
         options: 'list',
         list: ['sem educação formal','até o ensino médio','ensino superior','pós-graduação'],
         category: 'perfil_respondente'
     },  
     {
-        id: 306,
+        id: 56,
         description: 'Em que estado você reside?',
         options: 'list',
-        list: ['Capital', 'Interior'],
+        list: ['AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'DF','ES','GO','MA','MT','MS','MG','PA','PB','PR','PE','PI','RJ','RN','RS','RO','RR','SC','SP','SE','TO' ],
         category: 'perfil_respondente'
     },  
     {
-        id: 307,
+        id: 57,
         description: 'Quantas pessoas no total moram em sua casa?',
         options: 'list',
         list: ['Uma pessoa / Moro sozinho (a)', 'Duas pessoas', 'Três pessoas','Quatro pessoas', 'Cinco ou mais pessoas'],
         category: 'perfil_respondente'
      },     
      {
-        id: 308,
+        id: 58,
         description: 'Quais tipos de mídias sociais você utiliza?',
         options: 'check-list',
         list: ['Twitter', 'Facebook', 'Instagram','Telegram','Wechat', 'WhatsApp', 'Linkedin', 'Nenhuma'],
         category: 'perfil_respondente'
      },     
      {
-        id: 309,
+        id: 59,
         description: 'Quais tipos de mensagens você consideraria compartilhar na mídia social (se alguma)?',
         options: 'check-list',
         list: ['Notícias de esportes', 'Notícias políticas', 'Celebridades','Notícias de negócio (tendências de mercado, oportunidades de emprego)','Ciência e tecnologia', 'Bem-estar e saúde', 'Humor, piadas, memes', 'Outras'],
         category: 'perfil_respondente'
      }, 
      {
-        id: 310,
+        id: 510,
         description: 'Com que frequência você verifica as mensagens mídias sociais?',
         options: 'check-list',
         list: ['BBC News', 'G1', 'Secom','OMS','CNN', 'TV Senado', 'Google News', 'Outros'],
@@ -325,180 +334,146 @@ var cognitiva_AOT = [
     }	
 ];
 
-
-/*var lideranca_partidaria = [
-    {
-        id: 801,
-        title: 'Abaixo estão algumas frases que você deve avaliar de acordo com sua opinão, indicando seu grau de concordância com elas. Use a escala de avaliação fornecida abaixo, na qual o ponto 1 significa que você discorda totalmente da frase, e o ponto 7 indica que você concorda totalmente com ela.',
-        description: 'O Presidente Bolsonaro é uma boa fonte de informação sobre o coronavírus.',
-        options: '1-7',
-        category: 'lideranca-partidaria'
-    },	
-    {
-        id: 802,
-	description: 'O Presidente Bolsonaro está fazendo o possível para prevenir que o coronavírus se espalhe.',
-        options: 'open',
-        category: 'lideranca-partidaria'
-    },
-    {
-        id: 803,
-        description: 'O Brasil é um dos países mais bem preparado em relação à epidemia causada pelo coronavírus.',
-        options: 'open',
-        category: 'lideranca-partidaria'
-    },
-    {
-        id: 804,
-        description: 'A responsabilidade pelo controle e implementação de medidas de prevenção ao COVID-19 é do Governador do estado e não do Presidente Bolsonaro.',
-        options: 'open',
-        category: 'lideranca-partidaria'
-    },
-    {
-        id: 805,
-        description: 'Eu concordo com as medidas desempenhadas pelo governo para conter o alastramento e consequências causadas pelo coronavírus.',
-        options: 'open',
-        category: 'lideranca-partidaria'
-    },
-    {
-        id: 806,
-        description: 'No geral, estou satisfeito com o governo do Presidente Jair Bolsonaro quanto as decisões para o controle da pandemia causada pelo COVID-19.',
-        options: 'open',
-        category: 'lideranca-partidaria'
-    }
-];*/
-
 var debias_mito_1 = [
     {
         id: 901,
-        title: 'Na sequência serão apresentadas algumas mensagens que circularam pela mídia social. Para cada uma delas você deve avaliar se é verdade de acordo com seu conhecimento e sua opinão a respeito.', 
-        description: 'Neste experimento você irá ler algumas declarações que constratam mitos e fatos sobre vacinas. Você pode passar por elas de acordo com seu próprio ritmo. Clique em “próximo” para seguir',
+        description: 'Você concorda com a mensagem disponibilizada?',
         options: 'list',
 	list: ['Sim','Não'],
 	image: 'images/mito-1.jpg',	  
     },	
     {
         id: 902,
-        description: 'Você concorda com a mensagem disponibilizada?',
-        options: 'list',
-	list: ['Sim','Não'],
-    },	
-    {
-        id: 903,
         description: 'Você compartilharia esta mensagem em sua rede social? (por exemplo, através do Facebook ou Whatsapp)"? ',
         options: 'list',
 	list: ['Sim','Não','Talvez'],
      }	
   ];
 
-  var debias_mito_2 = [
+var debias_mito_1 = [
     {
-        id: 901,
-        title: 'Na sequência serão apresentadas algumas mensagens que circularam pela mídia social. Para cada uma delas você deve avaliar se é verdade de acordo com seu conhecimento e sua opinão a respeito.', 
-        description: 'De arcordo com o seu conhecimento, esta mensagem é verdadeira ou falsa?',
-        options: 'list',
-	list: ['Verdadeira','Falsa'],
-	image: 'images/gripezinha_bolsonaro',	  
-    },	
-    {
-        id: 902,
+        id: 903,
         description: 'Você concorda com a mensagem disponibilizada?',
         options: 'list',
 	list: ['Sim','Não'],
+	image: 'images/mito-1.jpg',	  
     },	
     {
-        id: 903,
+        id: 904,
         description: 'Você compartilharia esta mensagem em sua rede social? (por exemplo, através do Facebook ou Whatsapp)"? ',
         options: 'list',
 	list: ['Sim','Não','Talvez'],
      }	
   ];
- var debias_mito_3 = [
+  
+var debias_mito_2 = [
     {
-        id: 901,
-        title: 'Na sequência serão apresentadas algumas mensagens que circularam pela mídia social. Para cada uma delas você deve avaliar se é verdade de acordo com seu conhecimento e sua opinão a respeito.', 
-        description: 'De arcordo com o seu conhecimento, esta mensagem é verdadeira ou falsa?',
-        options: 'list',
-	list: ['Verdadeira','Falsa'],
-	image: 'images/covid-19 - criado em laboratorio',	  
-    },	
-    {
-        id: 902,
+        id: 905,
         description: 'Você concorda com a mensagem disponibilizada?',
         options: 'list',
 	list: ['Sim','Não'],
+	image: 'images/mito-2.jpg',	  
     },	
     {
-        id: 903,
+        id: 906,
         description: 'Você compartilharia esta mensagem em sua rede social? (por exemplo, através do Facebook ou Whatsapp)"? ',
         options: 'list',
 	list: ['Sim','Não','Talvez'],
      }	
   ];
- var vies_confirmacao_4 = [
+  
+var debias_mito_3 = [
     {
-        id: 901,
-        title: 'Na sequência serão apresentadas algumas mensagens que circularam pela mídia social. Para cada uma delas você deve avaliar se é verdade de acordo com seu conhecimento e sua opinão a respeito.', 
-        description: 'De arcordo com o seu conhecimento, esta mensagem é verdadeira ou falsa?',
-        options: 'list',
-	list: ['Verdadeira','Falsa'],
-	image: 'images/ivermectina'	  
-    },	
-    {
-        id: 902,
+        id: 907,
         description: 'Você concorda com a mensagem disponibilizada?',
         options: 'list',
 	list: ['Sim','Não'],
+	image: 'images/mito-3.jpg',	  
     },	
     {
-        id: 903,
+        id: 908,
         description: 'Você compartilharia esta mensagem em sua rede social? (por exemplo, através do Facebook ou Whatsapp)"? ',
         options: 'list',
 	list: ['Sim','Não','Talvez'],
      }	
   ];
-var vies_confirmacao_5 = [
+  
+var debias_mito_4 = [
     {
-        id: 901,
-        title: 'Na sequência serão apresentadas algumas mensagens que circularam pela mídia social. Para cada uma delas você deve avaliar se é verdade de acordo com seu conhecimento e sua opinão a respeito.', 
-        description: 'De arcordo com o seu conhecimento, esta mensagem é verdadeira ou falsa?',
-        options: 'list',
-	list: ['Verdadeira','Falsa'],
-	image: 'images/vitamina_D'	  
-    },	
-    {
-        id: 902,
+        id: 910,
         description: 'Você concorda com a mensagem disponibilizada?',
         options: 'list',
 	list: ['Sim','Não'],
+	image: 'images/mito-4.jpg',	  
     },	
     {
-        id: 903,
+        id: 911,
         description: 'Você compartilharia esta mensagem em sua rede social? (por exemplo, através do Facebook ou Whatsapp)"? ',
         options: 'list',
 	list: ['Sim','Não','Talvez'],
      }	
   ];
-
-var vies_confirmacao_6 = [
+  
+var debias_mito_5 = [
     {
-        id: 901,
-        title: 'Na sequência serão apresentadas algumas mensagens que circularam pela mídia social. Para cada uma delas você deve avaliar se é verdade de acordo com seu conhecimento e sua opinão a respeito.', 
-        description: 'De arcordo com o seu conhecimento, esta mensagem é verdadeira ou falsa?',
-        options: 'list',
-	list: ['Verdadeira','Falsa'],
-	image: 'images/vitamina_D'	  
-    },	
-    {
-        id: 902,
+        id: 912,
         description: 'Você concorda com a mensagem disponibilizada?',
         options: 'list',
 	list: ['Sim','Não'],
+	image: 'images/mito-5.jpg',	  
     },	
     {
-        id: 903,
+        id: 913,
         description: 'Você compartilharia esta mensagem em sua rede social? (por exemplo, através do Facebook ou Whatsapp)"? ',
         options: 'list',
 	list: ['Sim','Não','Talvez'],
      }	
   ];
-
-
+  
+var debias_mito_6 = [
+    {
+        id: 914,
+        description: 'Você concorda com a mensagem disponibilizada?',
+        options: 'list',
+	list: ['Sim','Não'],
+	image: 'images/mito-6.jpg',	  
+    },	
+    {
+        id: 915,
+        description: 'Você compartilharia esta mensagem em sua rede social? (por exemplo, através do Facebook ou Whatsapp)"? ',
+        options: 'list',
+	list: ['Sim','Não','Talvez'],
+     }	
+  ];
+  
+var debias_mito_7 = [
+    {
+        id: 916,
+        description: 'Você concorda com a mensagem disponibilizada?',
+        options: 'list',
+	list: ['Sim','Não'],
+	image: 'images/mito-7.jpg',	  
+    },	
+    {
+        id: 917,
+        description: 'Você compartilharia esta mensagem em sua rede social? (por exemplo, através do Facebook ou Whatsapp)"? ',
+        options: 'list',
+	list: ['Sim','Não','Talvez'],
+     }	
+  ];
+  
+var questions_intencao_vacina_depois= [    
+    {
+        id: 201,
+        title: 'Abaixo estão algumas frases que você deve avaliar de acordo com sua opinião. Use a escala de avaliação fornecida abaixo, na qual o ponto 1 significa que você discorda totalmente da frase, e o ponto 7 indica que você concorda totalmente com ela',
+        description: 'Qual a sua intenção de  tomar a vacina e vacinar sua família contra a covid-19 quando estiver disponível?',
+        options: '1-7',
+        category: 'intencao_depois'
+    },
+    {
+        id: 202,
+        description: 'Baseado em seus conhecimentos, quão provável que ao se vacinar contra a covid-19 você venha a sofrer complicações de saúde?',
+        options: '1-7',
+        category: 'complicacoes_depois'
+    }
+  ];
